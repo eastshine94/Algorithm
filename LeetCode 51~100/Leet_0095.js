2c19fc2da18d75cbade6db92434c41cb5f1c4207
KimHeeDong <eastshine94@naver.com>
2020-11-08 23:37:14 +0900

    201108

--- LeetCode 51~100/Leet_0095.js
+++ LeetCode 51~100/Leet_0095.js
@@ -1,19 +1,26 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {number} n
- * @return {TreeNode[]}
- */
-
 var generateTrees = function(n) {
+    
+    if (n==0) return [];
+    return createBST(1, n);
 };
 
-const getBST = (n, count) => {
-    
+
+const createBST = (start, end) => {
+        
+    if (start > end) return [null];
+    const arrBST = [];
+
+    for (let i = start; i <= end; i++) {
+        
+        let leftTree = createBST(start, i-1);
+        let rightTree = createBST(i+1, end);
+
+        for (let left of leftTree) {
+            for (let right of rightTree) {
+                const tree = new TreeNode(i, left, right);
+                arrBST.push(tree);
+            } 
+        }  
+    }
+    return arrBST;
 }
