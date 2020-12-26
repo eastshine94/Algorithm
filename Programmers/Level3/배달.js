965e545b153042da6ab98166243d8735e3440395
Heedong Kim <hdkim@joara.com>
2020-12-26 17:53:41 +0900

    201226

---
+++ Programmers/Level3/배달.js
@@ -0,0 +1,45 @@
+function solution(N, road, K) {
+  const set = new Set();
+  const minDists = new Array(N).fill(999999999);
+  minDists[0] = 0;
+  const roadArr = Array.from(Array(N), () => new Array(N).fill(999999999));
+  for (let val of road) {
+    const x = val[0] - 1;
+    const y = val[1] - 1;
+    const v = Math.min(roadArr[x][y], val[2]);
+    roadArr[x][y] = v;
+    roadArr[y][x] = v;
+  }
+  let selectedMinDist = 0;
+  let i = 0;
+  let count = 0;
+  while (count < N) {
+    let min = 999999999;
+    let minIdx = 0;
+    for (let j = 1; j < N; j++) {
+      if (set.has(j)) continue;
+      const dist = roadArr[i][j] + selectedMinDist;
+      const currDist = minDists[j];
+      if (dist >= 999999999) {
+        if (min > currDist) {
+          min = currDist;
+          minIdx = j;
+        }
+        continue;
+      }
+      if (currDist > dist) {
+        minDists[j] = dist;
+      }
+      if (min > minDists[j]) {
+        min = minDists[j];
+        minIdx = j;
+      }
+    }
+    set.add(minIdx);
+    selectedMinDist = min;
+    i = minIdx;
+    count++;
+  }
+
+  return minDists.filter(val => val <= K).length;
+}
